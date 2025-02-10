9a451533b02dbf6d65646118a4a582816f781299
johnhm22 <johnhowardmorgan@outlook.com>
2025-02-10 15:16:35 +0000

    Refactoring by replacing code with components

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,25 @@
 import Image from "next/image";
 import Card from "./components/card";
 import Link from "next/link";
+import Myskills from "./components/myskills";
+import Myprojects from "./components/myprojects";
 
 export default function Home() {
   return (
     <>
-    <div className="container text-gray-500 mx-auto min-h-screen border border-red-500 flex items-center">
-      <div className="border border-orange-400 flex flex-col">
+    <div className="container  mx-auto min-h-screen flex items-center">
+      <div className="flex flex-col">
       <h1 className="text-7xl mb-11">Welcome to my web page</h1>      
-      <div className="border border-blue-500 h-80 ">
-        <h2 className="text-5xl mb-9">My name is John...</h2>
-      <p className="text-3xl text-gray-400">I am a UK-based fullstack developer. My career so far has involved working in different continents and countries in customer-facing roles delivering business-critical software systems. Today, I enjoy the satisfaction of delivering web apps to clients.</p>
+      <div className="h-80 ">
+        <h2 className="text-5xl mb-9 font-normal">My name is John...</h2>
+      <p className="text-3xl leading-10">I am a UK-based fullstack developer. My career so far has involved working on different continents in customer-facing roles delivering business-critical software systems. Some years ago I put myself through a tough software engineering bootcamp with <Link href='https://www.springboard.com/courses/software-engineering-career-track/' target='_blank' >Springboard.</Link> Today, I enjoy the satisfaction of delivering web apps to clients.</p>
       </div>
       </div>
       </div>
 
-   <div className="container text-gray-500 mx-auto min-h-screen border border-red-500 flex">
-      <div className="border border-orange-400 flex flex-col">
-          
-      <div className="border border-blue-500 h-80 ">
-        <h2 className="text-5xl mb-9">My Skills</h2>
-      <p className="text-3xl text-gray-400">I am a UK-based fullstack developer. My career so far has involved working in different continents and countries in customer-facing roles delivering business-critical software systems. A few years ago I put myself through an intensive software engineer bootcamp with <Link href='https://www.springboard.com/courses/software-engineering-career-track/'>Springboard</Link>. Today, I enjoy the satisfaction of delivering web apps to clients.</p>
-      </div>
-      </div>
-      </div>
-      {/* <Card  src='/projects/finance-app' title='Finance App' description='Next.js 13 had come out recently and I was keen to use this, make lots of mistakes, get confused and so learn. Also, I had not used Tailwind to any great degree so this was a great opportunity to do so.'/> */}
-          
-         
+<Myskills />
+
+<Myprojects/>
           </>
           
   );
