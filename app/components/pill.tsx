3c00a3c98fadfd8b4779becd76b780aac5514cc4
johnhm22 <johnhowardmorgan@outlook.com>
2025-02-10 16:57:06 +0000

    Updating file to correct naming convention

--- app/components/pill.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const pill = ({text}: {text: string}) => {
-  return (
-    <div className=' inline-flex shadow-lg mx-3 my-3 w-md rounded-md py-2 px-3 bg-gray-300 font-normal text-black'>{text}</div>
-  )
-}
-
-export default pill
