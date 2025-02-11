0c8f5193a34bc9566090259e058ab2cb895d3c9a
johnhm22 <johnhowardmorgan@outlook.com>
2025-02-11 10:53:30 +0000

    Updating url for header images

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,22 +3,22 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='flex justify-between py-5 px-3 bg-blue-300'>
+    <div className='flex justify-between py-5 px-3 z-100 bg-slate-50 fixed min-w-full'>
             <div className='flex flex-row gap-2'>
             <Link href='/' target='_blank'>
-<img src='home.png' height={20} width={20}/>
+<img src='/home.png' height={20} width={20}/>
     </Link>
 <div>johnmorgan</div>
 </div>
 <div className='flex gap-5'>
     <Link href='' target='_blank'>
-<img src='inbox.png' height={20} width={20}/>
+<img src='/inbox.png' height={20} width={20}/>
     </Link>
     <Link href='https://www.linkedin.com/in/john-morgan-a6a1632' target='_blank'>
-<img src='linkedin.png' height={20} width={20}/>
+<img src='/linkedin.png' height={20} width={20}/>
     </Link>
     <Link href='https://github.com/johnhm22' target='_blank'>
-<img src='github.png' height={20} width={20}/>
+<img src='/github.png' height={20} width={20}/>
     </Link>
 </div>
 
