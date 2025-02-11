9a3db850fc376b22f8f3a8f891defdbfac0c4880
johnhm22 <johnhowardmorgan@outlook.com>
2025-02-11 19:51:46 +0000

    Adding mailto href for email icon.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,29 +1,36 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
   return (
-    <div className='flex justify-between py-5 px-3 z-100 bg-slate-50 fixed min-w-full'>
-            <div className='flex flex-row gap-2'>
-            <Link href='/' target='_blank'>
-<img src='/home.png' height={20} width={20}/>
-    </Link>
-<div>johnmorgan</div>
-</div>
-<div className='flex gap-5'>
-    <Link href='' target='_blank'>
-<img src='/inbox.png' height={20} width={20}/>
-    </Link>
-    <Link href='https://www.linkedin.com/in/john-morgan-a6a1632' target='_blank'>
-<img src='/linkedin.png' height={20} width={20}/>
-    </Link>
-    <Link href='https://github.com/johnhm22' target='_blank'>
-<img src='/github.png' height={20} width={20}/>
-    </Link>
-</div>
+    <nav>
+      <div className="relative flex justify-between py-5 px-3 z-100 bg-slate-50 min-w-full">
+        <Link href="/" className="flex flex-row gap-3">
+          <img src="/home.png" height={10} width={20} />
 
-    </div>
-  )
-}
+          <div>johnmorgan</div>
+        </Link>
 
-export default Header
+        <div className="flex gap-5">
+          <Link
+            href="mailto:johnhowardmorgan@outlook.com?subject=Query from your website"
+            target="_blank"
+          >
+            <img src="/inbox.png" height={20} width={20} />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/john-morgan-a6a1632"
+            target="_blank"
+          >
+            <img src="/linkedin.png" height={20} width={20} />
+          </Link>
+          <Link href="https://github.com/johnhm22" target="_blank">
+            <img src="/github.png" height={20} width={20} />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
