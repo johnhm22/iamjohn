179fad115acae4c91d233344bf3a76d66b5d1f7e
johnhm22 <johnhowardmorgan@outlook.com>
2025-02-13 11:25:23 +0000

    Adding component

---
+++ app/components/FreeStuff.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import React from "react";
+
+const FreeStuff = () => {
+  return (
+    <div      
+      id="free"
+      className="container mx-auto min-h-maxborder border-red-500 flex flex-col items-center my-16 text-center"
+    >
+      <div className="flex flex-row gap-3">
+        <img
+          className="object-contain"
+          src="/giftbox.png"
+          height={30}
+          width={30}
+        />{" "}
+        <h2 className="text-5xl font-semibold">Free Stuff</h2>
+        <img
+          className="object-contain"
+          src="/giftbox.png"
+          height={30}
+          width={30}
+        />
+      </div>
+      <p className="w-1/2 mt-3 leading-6">
+        If you need help on a project, a couple of days' coding for example,
+        based on the skill set I can offer then{" "}
+        <Link
+          href="mailto:johnhowardmorgan@outlook.com?subject=Query from your website"
+          target="_blank"
+          
+        >
+          please get in touch.
+          <>
+            <img
+              className="inline object-contain"
+              src="/inbox.png"
+              height={20}
+              width={20}
+            />
+          </>
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default FreeStuff;
