01ce3808d70250395330a90e60f97f9de92d0b66
johnhm22 <johnhowardmorgan@outlook.com>
2025-02-13 11:26:02 +0000

    Adding dialog element

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,17 +1,47 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import React, { useRef } from "react";
+import Image from "next/image";
+import FreeStuff from "./FreeStuff";
 
 const Header = () => {
+  const dialogRef = useRef<HTMLDialogElement | null>(null);
+
+  const handleOnClick = () => {
+    console.log("handleOnClick called");
+    dialogRef.current?.showModal();
+  };
+
+  const closeDialog = () => {
+    dialogRef.current?.close();
+  };
+
   return (
     <nav>
       <div className="relative flex justify-between py-5 px-3 z-100 bg-slate-50 min-w-full">
         <Link href="/" className="flex flex-row gap-3">
-          <img src="/home.png" height={10} width={20} />
-
+          <Image
+            className="object-contain"
+            alt="home page icon"
+            src="/home.png"
+            height={20}
+            width={20}
+          />
           <div>johnmorgan</div>
         </Link>
 
-        <div className="flex gap-5">
+        <div className="flex gap-5 cursor-pointer">
+          <div className="flex flex-row gap-2 mr-3" onClick={handleOnClick}>
+            <p className="text-sm italic hover:font-semibold">Free help</p>
+            <img
+              className="object-contain"
+              src="/giftbox.png"
+              height={20}
+              width={20}
+            />
+          </div>
+
           <Link
             href="mailto:johnhowardmorgan@outlook.com?subject=Query from your website"
             target="_blank"
@@ -29,6 +59,15 @@ const Header = () => {
           </Link>
         </div>
       </div>
+      <dialog ref={dialogRef} className="relative w-2/5 backdrop-blur-xl">
+        <button
+          onClick={closeDialog}
+          className="absolute border text-gray-400 font-normal text-sm top-1 right-1 rounded-md px-2 py-1 text-md bg-gray-200"
+        >
+          esc
+        </button>
+        <FreeStuff />
+      </dialog>
     </nav>
   );
 };
