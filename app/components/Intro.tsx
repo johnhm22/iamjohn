5b5a22a4dd004576f2721d1fee52b72747dbb632
johnhm22 <johnhowardmorgan@outlook.com>
2025-02-13 19:19:54 +0000

    Making min-h-screen apply to all sizes

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 const Intro = () => {
   return (
-    <div className="w-11/12 mx-auto md:min-h-screen flex items-center mb-11">
+    <div className="w-11/12 mx-auto min-h-screen flex items-center mb-11">
       <div className="flex flex-col">
         <h1 className="text-4xl lg:text-7xl mb-11">Welcome to my web page</h1>
-        <div className="h-80 ">          
+        <div className="h-80 ">
           <h2 className="heading">My name is John...</h2>
           <p className="text-xl lg:text-3xl leading-8 lg:leading-10">
             I am a UK-based fullstack developer. My career so far has involved
