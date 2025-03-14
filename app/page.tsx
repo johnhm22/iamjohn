cc6651edfd47c9471caf861a68fcfd31f4bd51a0
johnhm22 <johnhowardmorgan@outlook.com>
2025-03-14 15:01:12 +0000

    Adding code to ensure app is mobile responsive

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Myprojects from "./components/MyProjects";
 
 export default function Home() {
   return (
-    <section className="px-9 grid grid-cols-3 gap-7">
-      <div className="mt-20 self-start sticky top-20">
+    <section className="px-9 grid grid-cols-1 md:grid-cols-3 gap-7">
+      <div className="mt-20 self-start md:sticky top-20">
         <Intro />
       </div>
       <div className="col-span-2 mt-20 overflow-auto">
