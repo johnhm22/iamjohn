4ecb337029df88c49fd6946c646c836013fdd08f
johnhm22 <johnhowardmorgan@outlook.com>
2025-04-01 16:36:27 +0100

    Adding a tailwindcss project

--- app/components/MyProjects.tsx
+++ app/components/MyProjects.tsx
@@ -2,51 +2,57 @@ import React from "react";
 import Card from "./Card";
 
 const MyProjects = () => {
-  return (
-    <div>
-      <div className=" mx-auto min-h-screen">
-        <h2 className="heading-section">Projects</h2>
-        <div className="h-full justify-items-center grid grid-cols-1 lg:grid-cols-2">
-          <Card
-            src="/finance-app.png"
-            title="Finance App"
-            description="Next.js 13 had come out recently and I was keen to use this, make lots of mistakes, get confused and so learn. Also, I had not used Tailwind to any great degree so this was a great opportunity to do so."
-            link="finance-app"
-          />
-          <Card
-            src="/prismic-home.png"
-            title="Prismic CMS with Next.js"
-            description="The application of the Prismic Content Management System along with Next.js to produce an app that can be customised by non-dev colleagues."
-            link="prismic"
-          />
-          <Card
-            src="/ai4.png"
-            title="OpenAI Multi-Language Chatbot"
-            description="An entertaining AI chatbot where you control the language of the reply. It uses the Vercel AI SDK and, in particular, the useChat hook. The AI model along with system prompt, maxTokens and temperature are coded into an api call"
-            link="language-chatbot"
-          />
-          <Card
-            src="/ai-object-detection-cars.png"
-            title="AI Object Detection"
-            description="Demonstrating AI in the important area of object detection. Using a xenova transformer model, an image is provided which the model then analyses and returns with key features identified. Quite an eye-opener to the power of AI."
-            link="object-detection"
-          />
-          <Card
-            src="/black-and-white.png"
-            title="AI Image-to-Image Transformation"
-            description="Using the power of AI to take an image as input and transform it based on instructions provided."
-            link="image-transformation"
-          />
-          <Card
-            src="/georgina-home.png"
-            title="Georgina Homes - proof of concept"
-            description="Applying Prismic CMS to a property rental. Using a variety of frameworks and libraries - Next.js, Prismic, Prisma, PostgreSQL, but also design/coding tools."
-            link="georgina-homes"
-          />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className=" mx-auto min-h-screen">
+				<h2 className="heading-section">Projects</h2>
+				<div className="h-full justify-items-center grid grid-cols-1 lg:grid-cols-2">
+					<Card
+						src="/finance-app.png"
+						title="Finance App"
+						description="Next.js 13 had come out recently and I was keen to use this, make lots of mistakes, get confused and so learn. Also, I had not used Tailwind to any great degree so this was a great opportunity to do so."
+						link="finance-app"
+					/>
+					<Card
+						src="/prismic-home.png"
+						title="Prismic CMS with Next.js"
+						description="The application of the Prismic Content Management System along with Next.js to produce an app that can be customised by non-dev colleagues."
+						link="prismic"
+					/>
+					<Card
+						src="/ai4.png"
+						title="OpenAI Multi-Language Chatbot"
+						description="An entertaining AI chatbot where you control the language of the reply. It uses the Vercel AI SDK and, in particular, the useChat hook. The AI model along with system prompt, maxTokens and temperature are coded into an api call"
+						link="language-chatbot"
+					/>
+					<Card
+						src="/ai-object-detection-cars.png"
+						title="AI Object Detection"
+						description="Demonstrating AI in the important area of object detection. Using a xenova transformer model, an image is provided which the model then analyses and returns with key features identified. Quite an eye-opener to the power of AI."
+						link="object-detection"
+					/>
+					<Card
+						src="/black-and-white.png"
+						title="AI Image-to-Image Transformation"
+						description="Using the power of AI to take an image as input and transform it based on instructions provided."
+						link="image-transformation"
+					/>
+					<Card
+						src="/georgina-home.png"
+						title="Georgina Homes - proof of concept"
+						description="Applying Prismic CMS to a property rental. Using a variety of frameworks and libraries - Next.js, Prismic, Prisma, PostgreSQL, but also design/coding tools."
+						link="georgina-homes"
+					/>
+					<Card
+						src="/tailwind-cats.png"
+						title="Tailwindcss - a pawtrons testimonial"
+						description="A tailwind challenge! A demo showing the application of the tailwindcss library to a testimonial page."
+						link="tailwindcss"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default MyProjects;
---
+++ app/projects/tailwindcss/page.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import Image from "next/image";
+import Pill from "@/app/components/Pill";
+import Link from "next/link";
+
+const page = () => {
+	return (
+		<>
+			<section className="flex justify-center">
+				<div className="w-11/12 pl-3 mt-14">
+					<h1 className="pt-7 text-5xl mb-9 font-normal">
+						Tailwindcss Challenge
+					</h1>
+					<div className="mb-7 ">
+						<p className="text-4xl">Purpose</p>
+						<p className="my-3">
+							An excerise to show the flexibility of the tailwindcss library.
+						</p>
+						<p className="my-3"></p>
+					</div>
+					<div className="mb-12">
+						<p className="text-4xl">What does it do?</p>
+						<p className="my-3">
+							It demonstrates the use of tailwindcss to code the appearance of a
+							mobile responsive testimonials page. As the size of the screen
+							changes, the presentation adapts accordingly providing a smooth,
+							impressive customer experience.
+						</p>
+						<p className="mb-3">
+							A coding challenge as part of a course on
+							https://scrimba.com/home. A wide range of tailwind properties are
+							used.
+						</p>
+						<ul
+							role="list"
+							className="list-disc marker:text-blue-500 pl-7 mb-11"
+						>
+							<li>Typography</li>
+							<li>Display and Position</li>
+							<li>Flexbox</li>
+							<li>Borders</li>
+							<li>Layout</li>
+							<li>Box shadow, opacity, fill, cursor, screen readers</li>
+							<li>Modifiers: hover, focus etc.</li>
+						</ul>
+						<p className="mb-5 text-xl">Desktop</p>
+						<div className="flex mt-5  justify-center">
+							<Image
+								alt="tailwindcss demo page"
+								width={800}
+								height={800}
+								className=""
+								src="/tailwind-cats.png"
+							/>
+						</div>
+						<p className="my-5 text-xl">Mobile</p>
+						<div className="flex mt-5  justify-center">
+							<Image
+								alt="tailwindcss demo page"
+								width={400}
+								height={400}
+								className=""
+								src="/tailwind-cats-mobile.png"
+							/>
+						</div>
+					</div>
+
+					<div className="mb-7">
+						<p className="text-4xl mb-3">Tech used</p>
+						<div>
+							<Pill text="HTML/css" />
+							<Pill text="Tailwindcss" />
+						</div>
+					</div>
+					<div className="flex justify-end gap-3 pb-3 pr-3 font-medium">
+						<Link
+							href="https://github.com/johnhm22/tailwind-cats"
+							target="_blank"
+						>
+							<button className="rounded-md py-2 px-4 bg-green-500">
+								Code
+							</button>{" "}
+						</Link>
+						<Link href="https://tailwind-cats.vercel.app/" target="_blank">
+							<button className="rounded-md py-2 px-4 bg-blue-500">
+								Website
+							</button>{" "}
+						</Link>
+					</div>
+				</div>
+			</section>
+		</>
+	);
+};
+
+export default page;
