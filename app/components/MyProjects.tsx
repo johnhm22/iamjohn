5d8ce2fd006e1dea80cf41ea59445cb558986872
johnhm22 <johnhowardmorgan@outlook.com>
2025-04-08 10:41:27 +0100

    Reordering list of projects

--- app/components/MyProjects.tsx
+++ app/components/MyProjects.tsx
@@ -20,6 +20,18 @@ const MyProjects = () => {
 						link="prismic"
 					/>
 					<Card
+						src="/invoice-homepage.png"
+						title="Next.js v15 invoice pagination app"
+						description="An app to demo and practice the Next.js server-side components and server actions to draw data from a PostgreSQL database for presentation in a tailwindcss coded frontend."
+						link="invoice-pagination"
+					/>
+					<Card
+						src="/georgina-home.png"
+						title="Georgina Homes - proof of concept"
+						description="Applying Prismic CMS to a property rental. Using a variety of frameworks and libraries - Next.js, Prismic, Prisma, PostgreSQL, but also design/coding tools."
+						link="georgina-homes"
+					/>
+					<Card
 						src="/ai4.png"
 						title="OpenAI Multi-Language Chatbot"
 						description="An entertaining AI chatbot where you control the language of the reply. It uses the Vercel AI SDK and, in particular, the useChat hook. The AI model along with system prompt, maxTokens and temperature are coded into an api call"
@@ -37,23 +49,12 @@ const MyProjects = () => {
 						description="Using the power of AI to take an image as input and transform it based on instructions provided."
 						link="image-transformation"
 					/>
-					<Card
-						src="/georgina-home.png"
-						title="Georgina Homes - proof of concept"
-						description="Applying Prismic CMS to a property rental. Using a variety of frameworks and libraries - Next.js, Prismic, Prisma, PostgreSQL, but also design/coding tools."
-						link="georgina-homes"
-					/>
+
 					<Card
 						src="/tailwind-cats.png"
 						title="Tailwindcss - a pawtrons testimonial"
 						description="A tailwind challenge! A demo showing the application of the tailwindcss library to a testimonial page."
 						link="tailwindcss"
-					/>
-					<Card
-						src="/invoice-homepage.png"
-						title="Next.js v15 invoice pagination app"
-						description="An app to demo and practice the Next.js server-side components and server actions to draw data from a PostgreSQL database for presentation in a tailwindcss coded frontend."
-						link="invoice-pagination"
 					/>
 				</div>
 			</div>
