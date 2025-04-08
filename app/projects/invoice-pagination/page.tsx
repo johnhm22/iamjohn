3255c9954c986bda3e8108113c15f84cc33e435d
johnhm22 <johnhowardmorgan@outlook.com>
2025-04-08 10:42:17 +0100

    Adding text on project

--- app/projects/invoice-pagination/page.tsx
+++ app/projects/invoice-pagination/page.tsx
@@ -27,6 +27,11 @@ const page = () => {
 							<Link href="https://fakery.dev/">Fakery website</Link> and the
 							resulting data used as a seed file to populate the database.
 						</p>
+						<p className="my-3">
+							Other features include adding a new invoice and deleting an
+							existing invoice. These actions will update the Neon PostgreSQL
+							database.
+						</p>
 					</div>
 					<div className="mb-12">
 						<p className="text-4xl">What does it do?</p>
@@ -56,11 +61,11 @@ const page = () => {
 						</p>
 						<p className="mb-3">
 							The file for the server-side action is in a data-access folder.
-							All functions calling the database are placed here, although there
-							is only one in this case. An authorisation check is placed in the
-							files in this folder. The purpose is to ensure that the
-							authorisation is not separated from where the database call is
-							made and so avoid any risk of an unprotected call being made.
+							For security, all functions calling the database are placed here.
+							An authorisation check is normally placed in the functions in this
+							file. The purpose is to ensure that the authorisation is not
+							separated from where the database call is made and so avoid any
+							risk of an unprotected call being made.
 						</p>
 						<p className="mb-3">
 							Of course, for this demo, there is no authorisation check as the
@@ -79,6 +84,14 @@ const page = () => {
 							/>
 						</div>
 						<p className="mt-5 mb-5 text-xl">Add Invoice</p>
+						<p className="mb-3">
+							This is a specific page and found on the url /add-invoice. It is
+							composed of a Next.js Form component. Upon clicking Add, a
+							function which is defined in the Form as an action prop is called.
+							This server-side function is found in the data-access folder. It
+							receives the values from the Form fields and sends them to the
+							PostgreSQL database using a Prisma method.
+						</p>
 						<div className="flex mt-5 justify-center">
 							<Image
 								alt="pagination app invoice add"
@@ -89,6 +102,14 @@ const page = () => {
 							/>
 						</div>
 						<p className="mt-5 mb-5 text-xl">Delete Invoice</p>
+						<p className="mb-3">
+							The image below shows a pop-up window which is a dialog element.
+							It appears when the red trash can icon is clicked. If the user
+							decides to proceed with the deletion, as with the add invoice
+							feature, a server-side function is called in the data-layer folder
+							which makes the deletion call to the database using a Prisma
+							method.
+						</p>
 						<div className="flex mt-5 justify-center">
 							<Image
 								alt="pagination app invoice add"
@@ -104,17 +125,27 @@ const page = () => {
 						<p className="text-4xl mb-3">Tech used</p>
 						<div>
 							<Pill text="HTML/css" />
-							<Pill text="Tailwindcss" />
+							<Link href="https://tailwindcss.com/" target="_blank">
+								<Pill text="Tailwindcss" />
+							</Link>
 							<Pill text="JavaScript" />
 							<Pill text="TypeScript" />
 						</div>
 						<div>
-							<Pill text="React v19" />
-							<Pill text="Next.js v15" />
+							<Link href="https://react.dev/" target="_blank">
+								<Pill text="React v19" />
+							</Link>
+							<Link href="https://nextjs.org/" target="_blank">
+								<Pill text="Next.js v15" />
+							</Link>
 						</div>
 						<div>
-							<Pill text="Prisma ORM" />
-							<Pill text="PostgreSQL" />
+							<Link href="</Link>" target="_blank">
+								<Pill text="Prisma ORM" />
+							</Link>
+							<Link href="https://neon.tech/" target="_blank">
+								<Pill text="PostgreSQL from Neon" />
+							</Link>
 						</div>
 					</div>
 					{/* <p className="text-4xl my-3">Lessons learnt</p>
